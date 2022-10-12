6fd5c10fd7cb2db9e9060d609285dc08cac1dba6
Sienvdb <sienvdbergh@gmail.com>
2022-10-12 12:40:58 +0200

    Feat: add orbit controls

--- lab4/main.js
+++ lab4/main.js
@@ -1,26 +1,30 @@
 import './style.css'
 
-const scene = new THREE.Scene();
-			const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-			const renderer = new THREE.WebGLRenderer();
-			renderer.setSize( window.innerWidth, window.innerHeight );
-			document.body.appendChild( renderer.domElement );
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  document.body.appendChild( renderer.domElement );
 
-			const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-			const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-			const cube = new THREE.Mesh( geometry, material );
-			scene.add( cube );
+  //orbit controls
+  const controls = new THREE.OrbitControls(camera, renderer.domElement);
 
-			camera.position.z = 5;
+  
+  const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+  const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+  const cube = new THREE.Mesh( geometry, material );
+  scene.add( cube );
 
-			function animate() {
-				requestAnimationFrame( animate );
+  camera.position.z = 5;
 
-				cube.rotation.x += 0.01;
-				cube.rotation.y += 0.01;
+  function animate() {
+    requestAnimationFrame( animate );
 
-				renderer.render( scene, camera );
-			};
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
 
-			animate();
+    renderer.render( scene, camera );
+  };
+
+  animate();
