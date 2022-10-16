399f6388069aed1455b7fa0cfe265bb2ff658450
Sienvdb <sienvdbergh@gmail.com>
2022-10-16 14:13:07 +0200

    feat: setup simple square

--- lab4/main.js
+++ lab4/main.js
@@ -1,4 +1,6 @@
 import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -8,9 +10,18 @@ import './style.css'
   document.body.appendChild( renderer.domElement );
 
   //orbit controls
-  const controls = new THREE.OrbitControls(camera, renderer.domElement);
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.enableDamping = true;
 
-  
+  //add sphere
+  const sphereGeometry = new THREE.SphereGeometry(100, 32, 32);
+  const sphereMaterial = new THREE.MeshStandardMaterial({color: 0xffffff});
+  //materiaal aan binnekant van bol weergeven zodat je precies in een wereld zit (geen zwarte achtergrond)
+  sphereMaterial.side = THREE.BackSide;
+  const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+  scene.add(sphere);
+
+
   const geometry = new THREE.BoxGeometry( 1, 1, 1 );
   const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
   const cube = new THREE.Mesh( geometry, material );
@@ -18,13 +29,5 @@ import './style.css'
 
   camera.position.z = 5;
 
-  function animate() {
-    requestAnimationFrame( animate );
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-
-    renderer.render( scene, camera );
-  };
-
-  animate();
+  renderer.render( scene, camera );
