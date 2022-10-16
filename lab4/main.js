e284990fbd9eb22138902707fd22d867e23b9ac3
Sienvdb <sienvdbergh@gmail.com>
2022-10-16 14:36:09 +0200

    feat: add globe with grass

--- lab4/main.js
+++ lab4/main.js
@@ -1,6 +1,7 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -13,21 +14,22 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
   const controls = new OrbitControls(camera, renderer.domElement);
   controls.enableDamping = true;
 
-  //add sphere
-  const sphereGeometry = new THREE.SphereGeometry(100, 32, 32);
-  const sphereMaterial = new THREE.MeshStandardMaterial({color: 0xffffff});
-  //materiaal aan binnekant van bol weergeven zodat je precies in een wereld zit (geen zwarte achtergrond)
-  sphereMaterial.side = THREE.BackSide;
-  const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-  scene.add(sphere);
+  //loader
+  const loader = new THREE.TextureLoader();
 
 
-  const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-  const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-  const cube = new THREE.Mesh( geometry, material );
+  const globe = new THREE.SphereGeometry(1, 32, 32);
+  const globeMaterial = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+  globeMaterial.map = loader.load('./textures/grass2.jpg');
+  const cube = new THREE.Mesh( globe, globeMaterial );
   scene.add( cube );
 
   camera.position.z = 5;
 
 
-  renderer.render( scene, camera );
+  function animate() {
+    requestAnimationFrame( animate );
+    renderer.render( scene, camera );
+  };
+  
+  animate();
