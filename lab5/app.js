9700b71a8f6b0bc46763f034d3bcf42fc4b02536
Sienvdb <sienvdbergh@gmail.com>
2022-10-19 12:47:45 +0200

    feat: add mongoose

--- lab5/app.js
+++ lab5/app.js
@@ -9,6 +9,10 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+//add mongoose
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/test');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
