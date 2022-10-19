989e3285faeb8fc0dfa5875edc04e33498c6e958
Sienvdb <sienvdbergh@gmail.com>
2022-10-19 15:00:03 +0200

    feat: install mongoose and mongod

--- lab5/app.js
+++ lab5/app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var messageRouter = require('./routes/messages');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', messageRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
