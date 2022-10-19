8d236c9901c71d1b1f7cfae95cc6bb81fdd4be79
Sienvdb <sienvdbergh@gmail.com>
2022-10-19 15:22:22 +0200

    feat: create getAll

---
+++ lab5/controllers/messages.js
@@ -0,0 +1,22 @@
+// require models
+const Message = require("../models/message");
+
+const getAll = (req, res) => {
+    Message.find({}, (err, messages) => {
+        if (err) { 
+            console.log(err)
+            let response = {
+                status: "error",
+                message: "Error creating topping"
+            }
+            res.json(response);
+        }
+        let response = {
+            status: "success",
+            data: messages
+        }
+        res.json(response);
+    });
+};
+
+module.exports.getAll = getAll;
--- lab5/routes/messages.js
+++ lab5/routes/messages.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const MessageController = require('../controllers/messages');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Siens message API' });
 });
+
+/* GET messages. */
+router.get('/messages', MessageController.getAll);
 
 module.exports = router;
