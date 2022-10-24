58e91294fdbe6ef7efca579fe10446d47ca76960
Sienvdb <sienvdbergh@gmail.com>
2022-10-24 10:08:05 +0200

    feat: add setup from codesandbox

---
+++ labo2/main.js
@@ -0,0 +1,10 @@
+import './style.css'
+
+// import another class
+import Bingo from "./src/bingo.js";
+
+// kickstart the app by calling the constructor via the `new` keyword
+const bingo = new Bingo();
+
+
+//START MET GIT COMMIT 'feat: startup project"
